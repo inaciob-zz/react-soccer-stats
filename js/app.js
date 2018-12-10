34e26e85da33c2042920b71851988494d6475aed
Brian Inacio <binacio@live.ca>
2018-12-10 16:33:58 -0500

    Adding basic ability to read in standings
    Using Premier League as hard-coded competition reference, have confirmed ability to read in standings & team data (wins, draws, losses & points totals). Needs to be adjusted so that competition data retrieval is dynamic based on competition from game user has selected

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ class SoccerSchedule extends React.Component {
 		defaultStatus: 'SCHEDULED',
 		activeState: 'SCHEDULED',
 		schedule: [],
+		standings: [],
+		standingsVisible: false,
 		today: new Date
 	}
 	// Default status for displayed matches is SCHEDULED
@@ -44,27 +46,75 @@ class SoccerSchedule extends React.Component {
 		const preFormattedStartTime = isoDate.slice(0, -1).split('T')[1];
 		return preFormattedStartTime.substring(0, preFormattedStartTime.lastIndexOf(':'))
 	}
+	getStandings = () => {
+		// TODO: Retrieve standings for the competition a user has requested to see the table for (remove hard-coding of PL as competition code)
+		axios({
+			url: 'https://api.football-data.org/v2/competitions/PL/standings?standingType=TOTAL',
+			method: 'get',
+			headers: {
+				'X-Auth-Token': config.apiKey
+			}
+		}).then(response => {
+			if(response.status == 200) {
+				this.setState({
+					loaded: true,
+					standings: response.data.standings[0].table,
+					standingsVisible: true
+				})
+			}
+		}).catch(function(error) {
+			console.log(error);
+		})
+	}
 	render() {
 		// Sort matches by league name first, then by start time
 		const sortedMatches = this.state.schedule
 								.sort((a, b) => a.competition.name > b.competition.name)
 								.map((match, index) => (
-									<div key={match.id} className={"match mt-4 p-4 " + this.formatClassName(match.competition.name)}>
-										<h2 className="match-league">{match.competition.name}</h2>
-									{/* TODO: Adjust this so that user local time is displayed */}
-										{match.status == this.state.defaultStatus && 
-											<p className="match-details">{match.homeTeam.name} vs {match.awayTeam.name} at {this.getStartTime(match.utcDate)}</p>
-										}
+									<div key={match.id} className="row match mt-4 p-4">
+										<div className={"col-sm-8 " + this.formatClassName(match.competition.name)}>
+											<h2 className="match-league">{match.competition.name}</h2>
+										{/* TODO: Adjust this so that user local time is displayed */}
+											{match.status == this.state.defaultStatus && 
+												<p className="match-details">{match.homeTeam.name} vs {match.awayTeam.name} at {this.getStartTime(match.utcDate)}</p>
+											}
 
-										{match.status == 'IN_PLAY' && 
-											<p className="match-details">{match.homeTeam.name} vs {match.awayTeam.name}</p>
-										}
+											{(match.status == 'IN_PLAY' || match.status == 'PAUSED') && 
+												<p className="match-details">{match.homeTeam.name} vs {match.awayTeam.name}</p>
+											}
 
-										{match.status == 'FINISHED' && 
-											<p className="match-details">{match.homeTeam.name} vs {match.awayTeam.name}</p>
-										}
+											{match.status == 'FINISHED' && 
+												<p className="match-details">{match.homeTeam.name} vs {match.awayTeam.name}</p>
+											}
+										</div>
+										<div className="col-sm-4">
+											<div className="row">
+												<div className="hoverable-icon" onClick={this.getStandings}>
+													<span className="hoverable-icon-desc">View Table</span>
+													<div className="clearfix"></div>
+													<i className="fas fa-table"></i>
+												</div>
+											</div>
+										</div>
 									</div>
 								))
+
+		const leagueTable = this.state.standings
+								.map((club, index) => (
+									<tr key={club.position}>
+								    	<th scope="row">{club.position}</th>
+								    	<td>
+								    		<img src={club.team.crestUrl} className="club-crest mr-4" />
+								    		{club.team.name}
+								    	</td>
+								    	<td>{club.playedGames}</td>
+								    	<td>{club.won}</td>
+								    	<td>{club.draw}</td>
+								    	<td>{club.lost}</td>
+								    	<td>{club.points}</td>
+								    </tr>
+								))
+
 
 		return (
 			<div className="container">
@@ -90,6 +140,27 @@ class SoccerSchedule extends React.Component {
 				}
 
 				{sortedMatches}
+
+				{this.state.standings.length > 0 && 
+					<div className="row mt-4">
+						<table className={"table table-bordered bg-white " + (this.state.standingsVisible ? 'visible' : 'invisible')}>
+							<thead className="thead-dark">
+								<tr>
+							      	<th scope="col">Position</th>
+							      	<th scope="col">Team</th>
+							      	<th scope="col">Played</th>
+							      	<th scope="col">Won</th>
+							      	<th scope="col">Drawn</th>
+							      	<th scope="col">Lost</th>
+							      	<th scope="col">Points</th>
+							    </tr>
+							</thead>
+							<tbody>
+								{leagueTable}
+							</tbody>
+						</table>
+					</div>
+				}
 
 				{/* TODO: Add retrieval of live/finished match data when that status is requested */}
 
